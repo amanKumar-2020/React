81fba1ef9532b8d0dce1078f122d470bee0cc672
Aman kumar <jnvaman4@gmail.com>
2026-03-29 17:51:42 +0530

    feat: enhance loginController with validation and error handling

--- project/backend/src/controller/auth.controller.js
+++ project/backend/src/controller/auth.controller.js
@@ -32,7 +32,7 @@ async function registerController(req, res, next) {
     });
 
     res.cookie("token", token);
-    
+
     res.status(201).json({
         success: true,
         message: "User registered successfully",
@@ -46,6 +46,41 @@ async function registerController(req, res, next) {
 async function loginController(req, res, next) {
   const { email, password, username } = req.body;
   try {
+    if ((!email && !username) || !password) {
+      return res.status(400).json({
+          success: false,
+          message: "Please provide email or username and password",
+        });
+    }
+    const user = await userModel.findOne({
+      $or: [{ email }, { username }]
+    }).select("+password");
+
+    if (!user) {
+      return res.status(400).json({
+          success: false,
+          message: "Invalid credentials",
+        });
+    }
+    const isPasswordMatch = await user.comparePassword(password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+          success: false,
+          message: "Invalid credentials",
+        });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "2d",
+    });
+
+    res.cookie("token", token);
+
+    res.json({
+        success: true,
+        message: "Logged in successfully",
+        user
+      });   
   } catch (error) {
     next(error);
   }
