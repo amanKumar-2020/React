7d07b0ba793c36c6b90bf11848e5cc43f8b2a9dc
Aman kumar <jnvaman4@gmail.com>
2026-06-01 20:14:45 +0530

    convert demo redux to actual redux , implement by parcel

--- ReduxToolkit/procodrr/01_Redux Fundamentals/script.js
+++ ReduxToolkit/procodrr/01_Redux Fundamentals/script.js
@@ -1,10 +1,13 @@
-let reduxState = {
+import {createStore, legacy_createStore} from "redux"
+console.dir(createStore)
+
+let initialState = {
   post: 0,
   name: "Aman",
   age: 23,
 };
 
-function reducer(state, action) {
+function reducer(state =initialState, action) {
   if (action.type === "post/increment") {
     return { ...state, post: state.post + 1 };
   } 
@@ -13,12 +16,13 @@ function reducer(state, action) {
   }
    else if (action.type === "post/incrementBy") {
      return { ...state, post: state.post - action.payload };
-   }
+   } return state
 }
-console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/increment" });
-console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/decrement" });
-console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/incrementBy" ,payload: 10 });
-console.log(reduxState)
+
+const store = createStore(reducer)
+console.log(store)
+
+console.log(store.getState());
+
+store.dispatch({ type: "post/decrement" });
+console.log(store.getState())
